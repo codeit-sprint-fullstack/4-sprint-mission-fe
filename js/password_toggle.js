448c754607fc0c591ec8a140d9736ee820a53b66
Hyeongmin Jo <paengdal1@gmail.com>
2024-10-26 22:21:24 +0900

    Update README.md to No.1 and Upload Mission 1,2 and Add togglePasswordVisibility function

---
+++ js/password_toggle.js
@@ -0,0 +1,22 @@
+// 눈동자 아이콘 요소들 선택
+const eyes=document.querySelectorAll('.eyes');
+
+// 이벤트 핸들러
+function togglePasswordVisibility(e){
+  
+  const imgSrc= e.target.getAttribute('src');
+
+  // 아이콘과 'input type' toggle 하기
+  if(imgSrc==="/img/eye_disable.png"){
+    e.target.setAttribute("src", "/img/eye.png");
+    e.target.parentElement.firstElementChild.setAttribute('type',"text")
+  }else{
+    e.target.setAttribute("src","/img/eye_disable.png");
+    e.target.parentElement.firstElementChild.setAttribute('type',"password")
+  }
+}
+
+// 눈동자 아이콘에 이벤트 리스너 등록
+for(let eye of eyes){
+  eye.addEventListener('click',togglePasswordVisibility);
+}
