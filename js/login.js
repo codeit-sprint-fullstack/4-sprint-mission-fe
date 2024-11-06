ac96e52adf64508b56a8a5a997b3ebdb416b0d6c
Hyeongmin Jo <paengdal1@gmail.com>
2024-11-06 09:57:40 +0900

    fix: Change naming rule, snake to kebab

--- js/login.js
+++ js/login.js
@@ -10,11 +10,11 @@ function togglePwVisibility(e) {
   const imgSrc = e.target.getAttribute('src');
 
   // 아이콘과 'input type' toggle 하기
-  if (imgSrc === "/img/eye_disable.png") {
+  if (imgSrc === "/img/eye-disable.png") {
     e.target.setAttribute("src", "/img/eye.png");
     e.target.parentElement.firstElementChild.removeAttribute('type');
   } else {
-    e.target.setAttribute("src", "/img/eye_disable.png");
+    e.target.setAttribute("src", "/img/eye-disable.png");
     e.target.parentElement.firstElementChild.setAttribute('type', "password");
   }
 }
