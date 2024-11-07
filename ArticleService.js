e9a3091058ec496cf3e0c53976e8b1b818ca08a9
Hyeongmin Jo <paengdal1@gmail.com>
2024-11-07 18:12:42 +0900

    feat: ArticleService.js에 createArticle, patchArticle, deleteArticle 함수 추가

--- ArticleService.js
+++ ArticleService.js
@@ -15,6 +15,21 @@ export async function getArticle(id) {
   return res.data;
 }
 
+export async function createArticle(article) {
+  const res = await instance.post('/articles', article);
+  return res.data;
+}
+
+export async function patchArticle(id, article) {
+  const res = await instance.patch(`/articles/${id}`, article);
+  return res.data;
+}
+
+export async function deleteArticle(id) {
+  const res = await instance.delete(`/articles/${id}`);
+  return res.data;
+}
+
 
 /* fetch로 구현 연습 */
 // export async function getArticleList(params = {}) {
@@ -33,4 +48,17 @@ export async function getArticle(id) {
 //   const res = await fetch(`https://sprint-mission-api.vercel.app/articles/${id}`);
 //   const data = await res.json();
 //   return data;
+// }
+
+// export async function createArticle(article) {
+//   const res = fetch('https://sprint-mission-api.vercel.app/articles', {
+//     method: 'POST',
+//     body: JSON.stringify(article),
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//   });
+//   const data = (await res).json();
+//   return data;
+
 // }
--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-import { getArticleList, getArticle } from "./ArticleService.js";
+import { getArticleList, getArticle, createArticle, patchArticle, deleteArticle } from "./ArticleService.js";
 
-const articles = await getArticleList({ page: 1, pageSize: 10, keyword: '안녕' });
+// const articles = await getArticleList({ page: 1, pageSize: 10, keyword: '안녕' });
 
-const article = await getArticle(131);
-
+const article = await getArticle(1049);
 console.log(article);
+
+// const arti = {
+//   title: "조형민 아티클",
+//   content: "코드잇 스프린트 최고!!",
+//   image: "이미지는 뭐지.png",
+// };
+// const newArticle = await createArticle(arti);
+// console.log(newArticle);
+
+// const arti = {
+//   title: "조형민 아티클 수정본",
+//   content: "코드잇 스프린트 최고!!",
+//   image: "이미지는 뭐지.png",
+// };
+// const patch = await patchArticle(1049, arti);
+
+// const deleteA = await deleteArticle(1050);
+// console.log(deleteA);
