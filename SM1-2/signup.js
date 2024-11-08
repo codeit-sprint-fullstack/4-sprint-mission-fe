1e93068c384dc1fb85ea6fd0e07dd6143a07c872
이현서 <lhs103725@gmail.com>
2024-11-08 16:39:50 +0900

    code update

--- SM1-2/signup.js
+++ SM1-2/signup.js
@@ -96,9 +96,24 @@ passwordConfirmInput.addEventListener('input', validateForm);
 // 폼 제출 함수
 function submitForm() {
   if (!validateForm()) {
-    alert('회원가입이 완료되었습니다.');
+    alert('회원가입이 완료되었습니다!');
     window.location.href = "/login";
     return true;
+  } else if (USER_DATA.some(user => user.email === email)) {
+    alert("이미 존재하는 이메일입니다!");
+    return false;
+  } else if (!emailPattern.test(email)) {
+    alert("잘못된 형식의 이메일입니다!");
+    return false;
+  } else if (password.length < 8) {
+    alert("비밀번호를 8자 이상 입력해주세요!");
+    return false;
+  } else if (!password) {
+    alert("비밀번호를 입력해주세요!");
+    return false;
+  } else if (passwordConfirm !== passwordInput.value.trim()) {
+    alert("비밀번호가 일치하지 않습니다!");
+    return false;
   }
 }
 
