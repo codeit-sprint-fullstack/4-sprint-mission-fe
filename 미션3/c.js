ea3f6b190f6ee7f9b5f2a14af0c18e4cf2a53d39
래훈 <fogns852@naver.com>
2024-11-11 17:05:03 +0900

    중간

---
+++ 미션3/c.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('#btn');
+btn.addEventListener('click',function(){
+  alert('환영합니다.')
+});
+
+let mail = document.querySelector('#email');
+mail.addEventListener('keydown');
