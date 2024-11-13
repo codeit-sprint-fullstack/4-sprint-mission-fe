2adc22523b7f105dd45c4ffbc8b5cc3b981fc383
Hyeongmin Jo <paengdal1@gmail.com>
2024-11-13 19:49:20 +0900

    fix: toggle-password.js의 이벤트 핸들러에 boolean변수 설정해보기

--- js/toggle-password.js
+++ js/toggle-password.js
@@ -2,17 +2,20 @@
 
 // 눈동자 아이콘 요소들 선택
 export const eyes = document.querySelectorAll(".eyes");
+let hidePassword = true;
 
 // 이벤트 핸들러
+
 export function togglePwVisibility(e) {
   const imgSrc = e.target.getAttribute("src");
+  hidePassword = !hidePassword;
 
   // 아이콘과 'input type' toggle 하기
-  if (imgSrc === "/img/eye-disable.png") {
-    e.target.setAttribute("src", "/img/eye.png");
-    e.target.parentElement.firstElementChild.removeAttribute("type");
-  } else {
+  if (hidePassword) {
     e.target.setAttribute("src", "/img/eye-disable.png");
     e.target.parentElement.firstElementChild.setAttribute("type", "password");
+  } else {
+    e.target.setAttribute("src", "/img/eye.png");
+    e.target.parentElement.firstElementChild.removeAttribute("type");
   }
 }
