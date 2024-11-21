1f94153cc26fa8659696c0874880d46879a1c14d
CisThard <he3717@gmail.com>
2024-11-21 16:36:40 +0900

    Add email/pw validation func in login.js

--- sprint_mission/login.js
+++ sprint_mission/login.js
@@ -1,3 +1,4 @@
+//PW 패스워드 가리기/가리지 않기
 document.addEventListener('DOMContentLoaded', function () {
     const passwordInput = document.querySelector('.login_section input[name="password"]');
     const eyeOpenPassword = document.getElementById('eye_open_password');
@@ -31,3 +32,47 @@ document.addEventListener('DOMContentLoaded', function () {
         eyeCloseConfirm.style.display = 'block';
     });
 });
+
+function EmailCheck(e) {
+    const inputElement = e.target;
+
+    if (inputElement.value === "") {
+        inputElement.style.border = '2px solid red';
+        emailError.style.display = 'block';
+
+    } else {
+        inputElement.style.border = 'none';
+    }
+}
+
+function PwCheck(e) {
+    const inputElement = e.target;
+
+    if (inputElement.value === "") {
+        inputElement.style.border = '2px solid red';
+        pwError.style.display = 'block';
+
+    } else {
+        inputElement.style.border = 'none';
+    }
+}
+
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("email_error");
+
+const pwInput = document.getElementById('password');
+const pwError = document.getElementById('pw_error')
+
+emailInput.addEventListener('focusin', function () {
+    emailInput.style.border = '';
+    emailError.style.display = 'none';
+});
+
+emailInput.addEventListener('focusout', EmailCheck);
+
+pwInput.addEventListener('focusin', function () {
+    pwInput.style.border = '';
+    pwError.style.display = 'none';
+});
+
+pwInput.addEventListener('focusout', PwCheck);
