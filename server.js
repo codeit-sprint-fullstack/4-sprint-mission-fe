c5085a97d8d25b59f3a99576d11cec721992aed0
Hyeongmin Jo <paengdal1@gmail.com>
2024-11-27 23:06:52 +0900

    feat: 상품 목록/상세 조회, 수정, 삭제 API 생성

--- server.js
+++ server.js
@@ -21,10 +21,56 @@ mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("Connected to DB"));
 
-// 상품 등록
+// 상품 등록 API
 app.post("/products", async (req, res) => {
   const product = await Product.create(req.body);
   res.status(201).send(product);
+});
+
+// 상품 상세 조회 API
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "해당 id의 상품을 찾을 수 없습니다." });
+  }
+});
+
+// 상품 목록 조회 API
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+
+// 상품 수정 API
+app.patch("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+
+  if (product) {
+    Object.keys(req.body).forEach((key) => {
+      product[key] = req.body[key];
+    });
+    await product.save();
+    res.status(201).send(product);
+  } else {
+    res.status(404).send({ message: "해당 id의 상품을 찾을 수 없습니다." });
+  }
+});
+
+// 상품 삭제 API
+app.delete("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findByIdAndDelete(id);
+
+  // 삭제에 성공하면 product, 실패하면 null을 리턴
+  if (product) {
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: "해당 id의 상품을 찾을 수 없습니다." });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
