c5b9d9cd34559e33f99397747fee2cfb9ca1ab3f
이현서 <lhs103725@gmail.com>
2024-12-03 11:54:44 +0900

    mobile fixed

--- panda-market/src/components/Footer.js
+++ panda-market/src/components/Footer.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={{ color:'black', padding: '10px', background: '#f5f5f5' }}>
+    <footer style={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      marginTop:'20px',
+      color:'white', 
+      padding: '32px 200px 32px 200px', 
+      backgroundColor: 'rgba(17, 24, 39, 1)',
+      }}>
       <p>© 2024 Panda Market</p>
     </footer>
   );
