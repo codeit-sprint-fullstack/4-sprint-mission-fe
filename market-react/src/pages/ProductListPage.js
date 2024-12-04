74225b1ce32bb49325b0cd5d586e668a033b5cbf
이현서 <lhs103725@gmail.com>
2024-12-05 01:31:21 +0900

    comment fixed

--- market-react/src/pages/ProductListPage.js
+++ market-react/src/pages/ProductListPage.js
@@ -9,7 +9,6 @@ import useDeviceSize from '../hooks/useDeviceSize.js';
 
 function ProductListPage() {
   const [items, setItems] = useState([]);
-  // const [bestItems, setBestItems] = useState([]);
   const [sort, setSort] = useState('recent'); // 정렬 옵션
   const [keyword, setKeyword] = useState(''); // 검색
   const [page, setPage] = useState(1); // pagination에 필요
@@ -36,18 +35,11 @@ function ProductListPage() {
     [keyword, page, sort]
   );
 
-  // const handleLoadBest = async (options) => {
-  //   const result = await getProducts(options);
-  //   const { list } = result;
-  //   setBestItems(list);
-  // };
+  
 
   const handleSubmit = (keyword) => {
     setKeyword(keyword);
-    /**
-     * 키워드 검색을 했을 때 페이지를 1로 변경하기
-     * - (문제 케이스) 4페이지에서 검색을 했는데 검색 결과의 페이지 수가 이보다 적을 경우 보이지 않음
-     */
+    
     if (keyword) {
       setPage(1);
     }
@@ -63,15 +55,7 @@ function ProductListPage() {
     });
   }, [sort, keyword, page, isTablet, isMobile, handleLoad]);
 
-  // 베스트 상품 목록 불러오기
-  // useEffect(() => {
-  //   handleLoadBest({
-  //     page: 1,
-  //     pageSize: isTablet ? 2 : isMobile ? 1 : 4,
-  //     orderBy: "favorite",
-  //   });
-  // }, [isTablet, isMobile]);
-
+  
   return (
     <div>
       <Header isProductPage={true} />
