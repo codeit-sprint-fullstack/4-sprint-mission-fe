8bfc7195585e445be6c5f78f41ca9ae1eab2bfea
Jinho Jeong <zinojung@users.noreply.github.com>
2025-01-15 11:03:39 +0900

    .next 디렉토리 제거

--- app/(root)/articles/[articleId]/page.jsx
+++ app/(root)/articles/[articleId]/page.jsx
@@ -1,10 +1,14 @@
+import api from "@/api";
 import React from "react";
 
 async function ArticlePage(props) {
   const params = await props.params;
   const articleId = params.articleId;
 
-  return <div>page {articleId}</div>;
+  const article = await api.getArticle(articleId);
+  console.log(article);
+
+  return <main>page {articleId}</main>;
 }
 
 export default ArticlePage;
