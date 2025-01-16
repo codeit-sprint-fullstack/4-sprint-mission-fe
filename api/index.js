7531cdb1078952a46c59b9c773542e6fabf91580
Hyeongmin Jo <paengdal1@gmail.com>
2025-01-16 12:15:23 +0900

    fix: api요청주소 렌더로 변경

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = 'https://four-sprint-mission-be.onrender.com/';
-const baseURL = 'http://localhost:5500';
+const baseURL = 'https://four-sprint-mission-be.onrender.com/';
+// const baseURL = 'http://localhost:5500';
 
 const client = axios.create({
   baseURL,
